59e80d4a8d4d75f2871f9230b84e56ded7d6c587
Rob <61951374+Leo4j@users.noreply.github.com>
2024-06-03 15:39:01 +0100

    Update Get-DNSRecords.ps1

--- Get-DNSRecords.ps1
+++ Get-DNSRecords.ps1
@@ -48,6 +48,140 @@ function DNSRecords {
     }
 }
 
+function FindDomainTrusts {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Domain,
+        [Parameter(Mandatory = $false)]
+        [string]$Server
+    )
+
+    # Define the TrustAttributes mapping
+    $TrustAttributesMapping = @{
+        [uint32]'0x00000001' = 'NON_TRANSITIVE'
+        [uint32]'0x00000002' = 'UPLEVEL_ONLY'
+        [uint32]'0x00000004' = 'FILTER_SIDS'
+        [uint32]'0x00000008' = 'FOREST_TRANSITIVE'
+        [uint32]'0x00000010' = 'CROSS_ORGANIZATION'
+        [uint32]'0x00000020' = 'WITHIN_FOREST'
+        [uint32]'0x00000040' = 'TREAT_AS_EXTERNAL'
+        [uint32]'0x00000080' = 'TRUST_USES_RC4_ENCRYPTION'
+        [uint32]'0x00000100' = 'TRUST_USES_AES_KEYS'
+        [uint32]'0x00000200' = 'CROSS_ORGANIZATION_NO_TGT_DELEGATION'
+        [uint32]'0x00000400' = 'PIM_TRUST'
+    }
+
+    try {
+        # Construct the LDAP path and create the DirectorySearcher
+        $ldapPath = if ($Server) { "LDAP://$Server/DC=$($Domain -replace '\.',',DC=')" } else { "LDAP://DC=$($Domain -replace '\.',',DC=')" }
+        $searcher = New-Object System.DirectoryServices.DirectorySearcher
+        $searcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+        $searcher.Filter = "(objectClass=trustedDomain)"
+        $searcher.PropertiesToLoad.AddRange(@("name", "trustPartner", "trustDirection", "trustType", "trustAttributes", "whenCreated", "whenChanged"))
+        
+        # Execute the search
+        $results = $searcher.FindAll()
+
+        # Enumerate the results
+        foreach ($result in $results) {
+            # Resolve the trust direction
+            $Direction = Switch ($result.Properties["trustdirection"][0]) {
+                0 { 'Disabled' }
+                1 { 'Inbound' }
+                2 { 'Outbound' }
+                3 { 'Bidirectional' }
+            }
+
+            # Resolve the trust type
+            $TrustType = Switch ($result.Properties["trusttype"][0]) {
+                1 { 'WINDOWS_NON_ACTIVE_DIRECTORY' }
+                2 { 'WINDOWS_ACTIVE_DIRECTORY' }
+                3 { 'MIT' }
+            }
+
+            # Resolve the trust attributes
+            $TrustAttributes = @()
+            foreach ($key in $TrustAttributesMapping.Keys) {
+                if ($result.Properties["trustattributes"][0] -band $key) {
+                    $TrustAttributes += $TrustAttributesMapping[$key]
+                }
+            }
+
+            # Create and output the custom object
+            $trustInfo = New-Object PSObject -Property @{
+                SourceName      = $Domain
+                TargetName      = $result.Properties["trustPartner"][0]
+                TrustDirection  = $Direction
+                TrustType       = $TrustType
+                TrustAttributes = ($TrustAttributes -join ', ')
+                WhenCreated     = $result.Properties["whenCreated"][0]
+                WhenChanged     = $result.Properties["whenChanged"][0]
+            }
+
+            $trustInfo
+        }
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
+    finally {
+        $searcher.Dispose()
+        if ($results) { $results.Dispose() }
+    }
+}
+
+function FindAllDomains{
+	$FindCurrentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+	if(!$FindCurrentDomain){$FindCurrentDomain = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.Trim()}
+	if(!$FindCurrentDomain){$FindCurrentDomain = $env:USERDNSDOMAIN}
+	if(!$FindCurrentDomain){$FindCurrentDomain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select Domain | Format-Table -HideTableHeaders | out-string | ForEach-Object { $_.Trim() }}
+		
+	$ParentDomain = ($FindCurrentDomain | Select-Object -ExpandProperty Forest | Select-Object -ExpandProperty Name)
+	$DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $ParentDomain)
+	$ChildContext = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+	$ChildDomains = @($ChildContext | Select-Object -ExpandProperty Children | Select-Object -ExpandProperty Name)
+		
+	$AllDomains = @($ParentDomain)
+		
+	if($ChildDomains){
+		foreach($ChildDomain in $ChildDomains){
+			$AllDomains += $ChildDomain
+		}
+	}
+		
+	# Trust Domains (save to variable)
+		
+	$TrustTargetNames = @(foreach($AllDomain in $AllDomains){(FindDomainTrusts -Domain $AllDomain).TargetName})
+	$TrustTargetNames = $TrustTargetNames | Sort-Object -Unique
+	$TrustTargetNames = $TrustTargetNames | Where-Object { $_ -notin $AllDomains }
+		
+	# Remove Outbound Trust from $AllDomains
+		
+	$OutboundTrusts = @(foreach($AllDomain in $AllDomains){FindDomainTrusts -Domain $AllDomain | Where-Object { $_.TrustDirection -eq 'Outbound' } | Select-Object -ExpandProperty TargetName})
+		
+	foreach($TrustTargetName in $TrustTargetNames){
+		$AllDomains += $TrustTargetName
+	}
+		
+	$AllDomains = $AllDomains | Sort-Object -Unique
+	
+	$AllDomains = $AllDomains | Where-Object { $_ -notin $OutboundTrusts }
+	
+	### Remove Unreachable domains
+
+	$ReachableDomains = $AllDomains
+
+	foreach($AllDomain in $AllDomains){
+		$ReachableResult = $null
+		$DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $AllDomain)
+		$ReachableResult = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+		if($ReachableResult){}
+		else{$ReachableDomains = $ReachableDomains | Where-Object { $_ -ne $AllDomain }}
+	}
+
+	$ReachableDomains
+}
+
 function Get-DNSRecords {
     param(
         [string]$Domain,
@@ -55,6 +189,20 @@ function Get-DNSRecords {
     )
     
     if($Domain){
+		Write-Host ""
+		Write-Host "Domain Controllers:"
+		$result = nslookup -type=all "_ldap._tcp.dc._msdcs.$Domain" 2>$null
+        $DomainControllers = @(($result | Where-Object { $_ -like '*svr hostname*' } | Select-Object -First 1).Split('=')[-1].Trim())
+		$FinalDCs = @()
+		$FinalDCs = foreach($dc in $DomainControllers){
+			[PSCustomObject]@{
+				Name             = $dc
+				Domain           = $Domain
+			}
+		}
+		
+		$FinalDCs | Sort-Object Domain,Name | ft -Autosize
+		
         # Convert the domain into LDAP path format
         $domainDN = "DC=" + ($Domain -replace "\.", ",DC=")
         $domainDNSZonesDN = "LDAP://DC=DomainDnsZones,$domainDN"
@@ -80,6 +228,24 @@ function Get-DNSRecords {
     }
     
     else{
+		$AllDomains = FindAllDomains
+		
+		Write-Host ""
+		Write-Host "Domain Controllers:"
+		$FinalDCs = @()
+		$FinalDCs = foreach ($Domain in $AllDomains){
+			$result = nslookup -type=all "_ldap._tcp.dc._msdcs.$Domain" 2>$null
+			$DomainControllers = @(($result | Where-Object { $_ -like '*svr hostname*' } | Select-Object -First 1).Split('=')[-1].Trim())
+			foreach($dc in $DomainControllers){
+				[PSCustomObject]@{
+					Name             = $dc
+					Domain           = $Domain
+				}
+			}
+		}
+		
+		$FinalDCs | Sort-Object Domain,Name | ft -Autosize
+		
         $rootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://RootDSE")
         $domainDN = $rootDSE.Properties["defaultNamingContext"].Value
         $forestDN = $rootDSE.Properties["rootDomainNamingContext"].Value
